42e2c6ab02290a06fe152de44bc8dd14e445a3b5
F4U57O <fabio.sciortino@helsinki.fi>
2023-09-22 13:52:46 +0300

    Update structure

---
+++ messages.py
@@ -0,0 +1,23 @@
+from db import db
+from sqlalchemy.sql import text
+import users
+
+def get_list():
+    sql = text("SELECT M.title, M.content, U.username, M.sent_at FROM messages M, users U WHERE M.user_id=U.id ORDER BY M.id")
+    result = db.session.execute(sql)
+    return result.fetchall()
+
+def send(title, content):
+    user_id = users.user_id()
+    if user_id == 0:
+        return False
+    sql = text("INSERT INTO messages (title, content, user_id, sent_at) VALUES (:title, :content, :user_id, NOW())")
+    db.session.execute(sql, {"title":title, "content":content, "user_id":user_id})
+    db.session.commit()
+    return True
+
+def result_query(query):
+    sql = text("SELECT M.title, M.content, U.username, M.sent_at FROM messages M, users U WHERE M.user_id=U.id AND M.content LIKE :query ORDER BY M.id")
+    result = db.session.execute(sql, {"query": "%" +query + "%"})
+    messages = result.fetchall()
+    return messages
