1aa9fe333c26963d3c11a0401936081fcdabe51d
F4U57O <fabio.sciortino@helsinki.fi>
2023-10-08 20:00:36 +0300

    Update schema

--- schema.sql
+++ schema.sql
@@ -1,7 +1,22 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username TEXT UNIQUE,
-    password TEXT
+    password TEXT,
+    role TEXT
+);
+CREATE TABLE areas (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    thread_count INTEGER DEFAULT 0,
+    message_count INTEGER DEFAULT 0,
+    last_message_time TIMESTAMP
+);
+CREATE TABLE threads (
+    id SERIAL PRIMARY KEY,
+    title TEXT,
+    content TEXT,
+    area_id INTEGER REFERENCES areas(id),
+    sent_at TIMESTAMP
 );
 CREATE TABLE messages (
     id SERIAL PRIMARY KEY,
@@ -11,14 +26,4 @@ CREATE TABLE messages (
     thread_id INTEGER REFERENCES threads(id),
     sent_at TIMESTAMP
 );
-CREATE TABLE threads (
-    id SERIAL PRIMARY KEY,
-    title TEXT,
-    content TEXT,
-    area_id INTEGER REFERENCES areas(id),
-    sent_at TIMESTAMP
-);
-CREATE TABLE areas (
-    id SERIAL PRIMARY KEY,
-    name TEXT
-);
+
